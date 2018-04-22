f06ecf7c4668cc41bb1cb54a2dd39a7f53e40666
hy yang <almond2yang@gmail.com>
2018-04-21 23:51:56 -0700

    server port

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     DBPATH: process.env.MONGODB_URI || 'mongodb://localhost:27017/agate-db',
-    PORT_EXPRESS : process.env.PORT || 2666,
+    PORT_EXPRESS : 2666,
     JWT_SECRET: 'growing-habbit',
     REGEX : {
         USERNAME : /^(?=.{4,20}$)[A-Za-z0-9]+(?:[_.][A-Za-z0-9]+)*$/, // 4-20, a-zA-Z0-9._, no._start or end, no consecutive ._
