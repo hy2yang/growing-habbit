88f8631c26183647139d8c01adcc52a565db72b5
hy yang <almond2yang@gmail.com>
2018-04-24 11:21:20 -0700

    bugfix: jwt secrest takes environment variable

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     DBPATH: process.env.MONGODB_URI || 'mongodb://localhost:27017/agate-db',
     PORT_EXPRESS : process.env.PORT || 2666,
-    JWT_SECRET: 'growing-habit',
+    JWT_SECRET: process.env.JWT_SECRET || 'growing-habit',
     ALLOWED_ORIGINS :['http://localhost', 'https://localhost', 'https://pages.github.com' , 'https://github.com'],
     REGEX : {
         USERNAME : /^(?=.{4,20}$)[A-Za-z0-9]+(?:[_.][A-Za-z0-9]+)*$/, // 4-20, a-zA-Z0-9._, no._start or end, no consecutive ._
